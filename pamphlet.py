38e572234d7c0b3cacca204b6600e53ac86ff0cc
isahiz <isabelle.hizon@gmail.com>
2019-09-29 15:08:35 -0400

    fixed image output

--- pamphlet.py
+++ pamphlet.py
@@ -5,14 +5,17 @@ import time
 import io
 import csv
 
-def begin_document(out):
+def begin_document(out, imgdir):
 	out.write("\\documentclass{article}\n")
 	out.write("\\usepackage[T1]{fontenc}\n")
 	out.write("\\usepackage[tableposition=top]{caption}\n")
 	out.write("\\usepackage{tabularx}\n")
 	out.write("\\usepackage{tikz}\n")
+	out.write("\\usepackage{graphicx}")
 
 	out.write("\n\\newcolumntype{a}{>{\hsize=0.3\hsize}X}\n\\newcolumntype{b}{>{\hsize=0.7\hsize}X}\n")
+
+	out.write("\\graphicspath{{"+ imgdir +"/}}")
 
 	out.write("\n\\begin{document}")
 
@@ -30,6 +33,14 @@ def end_table(out):
 	out.write("}\n")
 	out.write("\\end{table}\n")
 
+def get_clean_text(s):
+	return re.sub(r'[^\x00-\x7f]+','', s)
+
+def clean_path(s):
+	s = s.replace(" ", "")
+	s = s.replace("\\", "/")
+	return s
+
 def write_table(incsv, imgdir, out):
 	with open(incsv) as csvfile:
 		readcsv = csv.reader(csvfile, delimiter=',')
@@ -38,25 +49,30 @@ def write_table(incsv, imgdir, out):
 		println = ""
 		for row in readcsv:
 			column_count += 1
-			lastname = row[0]
+			lastname = get_clean_text(row[0])
+			print(lastname)
 			firstname = row[1]
 			extratxt = row[2]
-			img_filename = imgdir + "/" + lastname + "_" + firstname + ".jpg"
+			print(clean_path(imgdir))
+			img_filename = clean_path(imgdir) + "/" + firstname + lastname + ".jpg"
 			println += "\\resizebox{1in}{1in}{"
 			println += "\\begin{tikzpicture}\n" + "\\node[anchor=south west,inner sep=0] at (0,0) {\\includegraphics[width=\\textwidth]{" + img_filename + "}};\n" + "\\end{tikzpicture}}\n" 
 			println += "& " + firstname + " " + lastname + "\n"
 			println += extratxt + "\n" #designation information can go here
-			if column_count == 2:
-				column_count = 1
+			if column_count == column_limit:
+				column_count = 0
 				println += "\\\\ \\hline\n"
 			else:
 				println += " & "
 		if column_count < 2:
-			println = println[1: -1 * len("\\\\ \\hline\n")] + " & & " + "\\\\ \\hline\n"
+			println = println[1: -1 * len(" & ")] + " & & " + "\\\\ \\hline\n"
+		print(println[1: -1 * len("\\\\ \\hline\n")])
+
+		
 		out.write(println)
 
 def make_pdf(f):
-	os.system("pdflatex " + f)
+	os.system("pdflatex -output-directory=" + os.getcwd() + " " + f)
 
 # run `pamphlet.py -ic [csv file name] -o [output.tex] -img [directory images for file]`
 def main():
@@ -73,7 +89,7 @@ def main():
 
 	outfile = open(outname, 'w')
 
-	begin_document(outfile)
+	begin_document(outfile, imgdir_name)
 	begin_table(outfile)
 	write_table(incsv, os.path.abspath(imgdir_name), outfile)
 	end_table(outfile)
