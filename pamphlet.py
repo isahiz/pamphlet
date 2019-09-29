f2fc3ed26e23e180aabe4593b900362f5bd73e41
isahiz <isabelle.hizon@gmail.com>
2019-09-29 09:43:44 -0400

    cmd file input

--- pamphlet.py
+++ pamphlet.py
@@ -8,6 +8,19 @@ import time
 import io
 
 def main():
+	outname = "none"
+	inxls = "none"
+	imgdr = "none"
+	for i in range(1, len(sys.argv), 2):
+		if sys.argv[i] == '-o':
+			outname = sys.argv[i + 1]
+		elif sys.argv[i] == "-ix":
+			inxls = sys.argv[i + 1]
+		elif sys.argv[i] == "-img":
+			imgdr = sys.argv[i + 1]
+
+	infile = open(inxls, 'r')
+	outfile = open(outname, 'w')
 
 
 if __name__ == '__main__':
